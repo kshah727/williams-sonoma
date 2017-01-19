994f667baaaac9b14789201de7f286222fd1040f
Krishna Shah <kshah727@gmail.com>
2017-01-18 18:13:23 -0800

    create date and day

--- src/main/java/com/sqa/ks/helpers/AppBasics.java
+++ src/main/java/com/sqa/ks/helpers/AppBasics.java
@@ -38,4 +38,13 @@ public class AppBasics {
 		response = scanner.nextLine();
 		return response;
 	}
+
+	public static void tellUserWhatDayIsToday() {
+		Calendar now = Calendar.getInstance();
+		System.out.println("Current date : " + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + "-"
+				+ now.get(Calendar.YEAR));
+		String[] strDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday" };
+		// Day_OF_WEEK starts from 1 while array index starts from 0
+		System.out.println("Current day is : " + strDays[now.get(Calendar.DAY_OF_WEEK) - 1]);
+	}
 }
