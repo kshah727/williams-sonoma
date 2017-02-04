de1fe9be845b29d73789fa32e27b370d5e943cbd
Krishna Shah <kshah727@gmail.com>
2017-02-04 15:54:36 -0800

    create BasicPage

---
+++ src/main/java/com/sqa/ks/helpers/BasicPage.java
@@ -0,0 +1,38 @@
+package com.sqa.ks.helpers;
+
+import org.apache.log4j.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class BasicPage {
+
+	private WebDriver driver;
+
+	private Logger log;
+
+	public BasicPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		this.log = Logger.getLogger(BasicTest.class);
+	}
+
+	public WebDriver getDriver() {
+		return this.driver;
+	}
+
+	public Logger getLog() {
+		return this.log;
+	}
+
+	public boolean isElementpresent(By by) {
+		return AutoBasics.isElementPresent(getDriver(), by);
+	}
+
+	public boolean takeScreenshot() {
+		return AutoBasics.takeScreenShot(getDriver(), "Nestle:");
+	}
+
+	public boolean takeScreenshot(String name) {
+		return AutoBasics.takeScreenShot(getDriver(), "Nestle:" + name);
+	}
+}
