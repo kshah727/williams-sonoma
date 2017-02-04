f769ac082cc54f67d872c3e654e3ed455c751b25
Krishna Shah <kshah727@gmail.com>
2017-02-04 15:59:54 -0800

    Create BasicTest

---
+++ src/test/java/com/sqa/ks/helpers/BasicTest.java
@@ -0,0 +1,65 @@
+package com.sqa.ks.helpers;
+
+import java.util.concurrent.*;
+
+import org.apache.log4j.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.safari.*;
+import org.testng.annotations.*;
+
+public class BasicTest {
+
+	private String baseUrl;
+
+	private WebDriver driver;
+
+	private Logger log;
+
+	public BasicTest(String baseUrl) {
+		super();
+		this.baseUrl = baseUrl;
+		this.log = Logger.getLogger(BasicTest.class);
+		this.log.info("created basic test object through constructor");
+	}
+
+	public String getBaseUrl() {
+		return this.baseUrl;
+	}
+
+	public WebDriver getDriver() {
+		return this.driver;
+	}
+
+	public Logger getLog() {
+		return this.log;
+	}
+
+	@BeforeMethod
+	public void setupChrome() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		this.driver = new ChromeDriver();
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		this.driver.get(this.baseUrl);
+	}
+
+	@BeforeMethod(enabled = false)
+	public void setupFirefox() {
+		this.driver = new FirefoxDriver();
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		this.driver.get(this.baseUrl);
+	}
+
+	@BeforeMethod(enabled = false)
+	public void setupSafari() {
+		this.driver = new SafariDriver();
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		this.driver.get(this.baseUrl);
+	}
+
+	@AfterMethod
+	public void teardown() {
+		this.driver.quit();
+	}
+}
