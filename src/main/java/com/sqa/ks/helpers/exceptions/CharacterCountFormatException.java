a1af0d355d0ea8e6ab8afd506462a54f402b6ab9
Krishna Shah <kshah727@gmail.com>
2017-02-11 13:54:34 -0800

    create various exceptions

---
+++ src/main/java/com/sqa/ks/helpers/exceptions/CharacterCountFormatException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: CharacterCountFormatException.java<br>
+ *
+ *   Shah, Krishna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 11, 2017
+ *   
+ */
+
+package com.sqa.ks.helpers.exceptions;
+
+
+/**
+ * CharacterCountFormatException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Shah, Krishna
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class CharacterCountFormatException extends Exception {
+}
---
+++ src/main/java/com/sqa/ks/helpers/exceptions/DataTypesMismatchException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: DataTypesMismatchException.java<br>
+ *
+ *   Shah, Krishna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 11, 2017
+ *   
+ */
+
+package com.sqa.ks.helpers.exceptions;
+
+
+/**
+ * DataTypesMismatchException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Shah, Krishna
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class DataTypesMismatchException extends Exception {
+}
