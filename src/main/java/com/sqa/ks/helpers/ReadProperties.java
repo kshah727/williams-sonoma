0611af23a429213b329c54287b388b9b8b31c83b
Krishna Shah <kshah727@gmail.com>
2017-02-13 18:42:54 -0800

    more update on helpers

---
+++ src/main/java/com/sqa/ks/helpers/ReadProperties.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: ReadProperties.java<br>
+ *
+ *   Shah, Krishna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 13, 2017
+ *   
+ */
+
+package com.sqa.ks.helpers;
+
+
+/**
+ * ReadProperties //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Shah, Krishna
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class ReadProperties {
+}
